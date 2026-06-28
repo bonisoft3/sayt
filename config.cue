9502c389f075779fca67f96b6cb8cb3c195cb1ec
davireis <davi@davi.eng.br>
2026-06-28 10:00:09 -0300

    Merge pull request #1417 from worldsense/release/sayt-v0.17.4
    release(sayt): v0.17.4
    
    GitOrigin-RevId: 2af64040f1f77a4cf3cef8491c842ef91fef4d94

--- config.cue
+++ config.cue
@@ -99,7 +99,7 @@ import "list"
 
 say: {
 	self: {
-		version: *"v0.17.3" | string & =~"^v[0-9]+\\.[0-9]+\\.[0-9]+.*$"
+		version: *"v0.17.4" | string & =~"^v[0-9]+\\.[0-9]+\\.[0-9]+.*$"
 		flags?:  string
 		verbs?: [...string]
 	}
--- saytw.ps1
+++ saytw.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = "Stop"
-$Version = if ($env:SAYT_VERSION) { $env:SAYT_VERSION } else { "v0.17.3" }
+$Version = if ($env:SAYT_VERSION) { $env:SAYT_VERSION } else { "v0.17.4" }
 if (-not ($Version.StartsWith("v")) -and $Version -ne "latest") {
     $Version = "v$Version"
 }
