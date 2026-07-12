6165c90447932cf7cbfd2b6f9b85feb52b05e64f
davireis <davi@davi.eng.br>
2026-07-12 08:56:56 -0300

    Merge pull request #1462 from worldsense/fix/sayt-cd-dirty-guard
    fix(sayt-cd): reset tracked drift before goreleaser (v0.20.2)
    
    GitOrigin-RevId: cab8935317d67e77a7bbeba7ad4a202788d1fadc

--- saytw.ps1
+++ saytw.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = "Stop"
-$Version = if ($env:SAYT_VERSION) { $env:SAYT_VERSION } else { "v0.20.1" }
+$Version = if ($env:SAYT_VERSION) { $env:SAYT_VERSION } else { "v0.20.2" }
 if (-not ($Version.StartsWith("v")) -and $Version -ne "latest") {
     $Version = "v$Version"
 }
