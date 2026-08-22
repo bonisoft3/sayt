75ab047d49033205c8cbf3b613e04b3be4b27cd4
davireis <davi@davi.eng.br>
2026-08-22 06:57:16 -0700

    Merge pull request #1618 from worldsense/release/sayt-v0.29.1
    release(sayt): v0.29.1
    
    GitOrigin-RevId: 995c1c54df724571deeab047796409dd56551379

--- saytw.ps1
+++ saytw.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = "Stop"
-$Version = if ($env:SAYT_VERSION) { $env:SAYT_VERSION } else { "v0.29.0" }
+$Version = if ($env:SAYT_VERSION) { $env:SAYT_VERSION } else { "v0.29.1" }
 if (-not ($Version.StartsWith("v")) -and $Version -ne "latest") {
     $Version = "v$Version"
 }
