85e0d6b6dabe904976d56dd3a8ad7cfd26ac10b8
davireis <davi@davi.eng.br>
2026-09-10 22:19:07 -0300

    Merge pull request #1723 from worldsense/release/sayt-v0.37.1
    release(sayt): v0.37.1
    
    GitOrigin-RevId: 0264cea8f361ceef15229d2ebe3e143f749b0b77

--- saytw.ps1
+++ saytw.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = "Stop"
-$Version = if ($env:SAYT_VERSION) { $env:SAYT_VERSION } else { "v0.37.0" }
+$Version = if ($env:SAYT_VERSION) { $env:SAYT_VERSION } else { "v0.37.1" }
 if (-not ($Version.StartsWith("v")) -and $Version -ne "latest") {
     $Version = "v$Version"
 }
